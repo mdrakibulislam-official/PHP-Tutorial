f56f34bea7c39f30739e05af8cd07672055dfde9
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-13 21:50:36 +0600

    Create var_dump.php

---
+++ var_dump.php
@@ -0,0 +1,14 @@
+<?php
+
+$product_name = "Tissue";
+$price = 40;
+$isAvaiable = true;
+
+var_dump($product_name);
+var_dump($price);
+var_dump($isAvaiable);
+
+echo "<br>";
+
+var_dump($product_name, $price, $isAvaiable);
+?>
