1704f06560d3b5b625aa420faa87bbc024e333a2
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-14 12:18:11 +0600

    Create assignment-operator.php

---
+++ assignment-operator.php
@@ -0,0 +1,24 @@
+<?php
+
+$number = 7;
+
+$number += 3; // $number=$number+3;
+echo "The sum = $number";
+echo "<br>";
+
+$number -= 3; // $number=$number-3;
+echo "The differance = $number";
+echo "<br>";
+
+$number *= 3; // $number=$number*3;
+echo "The mul = $number";
+echo "<br>";
+
+$number /= 3; // $number=$number/3;
+echo "The div = $number";
+echo "<br>";
+
+$number %= 3; // $number=$number%3;
+echo "The mod = $number";
+
+?>
