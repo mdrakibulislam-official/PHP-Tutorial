f3b2e37ce6a788a5557c97d3b9817c55f26c18a8
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-14 12:08:02 +0600

    Create operator.php

---
+++ operator.php
@@ -0,0 +1,25 @@
+<?php
+
+$first_number = 7;
+$second_number = 3;
+
+$sum = $first_number + $second_number;
+echo "The sum = $sum";
+echo "<br>";
+
+$sub = $first_number - $second_number;
+echo "The differance = $sub";
+echo "<br>";
+
+$mul = $first_number * $second_number;
+echo "The mul = $mul";
+echo "<br>";
+
+$div = $first_number / $second_number;
+echo "The div = $div";
+echo "<br>";
+
+$mod = $first_number % $second_number;
+echo "The mod = $mod";
+
+?>
