4b1525238d52731013a121cb3762c3159bf206ff
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-14 11:34:57 +0600

    Create printf.php

---
+++ printf.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "md. rakibul islam";
+$new_name = ucwords($name);
+
+echo "$name"."<br>"." $new_name";
+printf("<br>");
+printf("Hi %s",ucwords($name));
+
+$first_name = "md. rakibul";
+$last_name = "islam";
+
+$html_block = "<div> 
+               <h1>%s</h1>
+               <h2>%s</h2>
+               </div>";
+printf($html_block, ucwords($first_name), ucwords($last_name));
+?>
