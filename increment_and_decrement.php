6c40263ee31b46185d42f7323de05d30635bdaee
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-15 15:16:15 +0600

    Create increment_and_decrement.php

---
+++ increment_and_decrement.php
@@ -0,0 +1,18 @@
+<?php
+
+$number = 7;
+echo $number++;// $number=7
+echo "<br>";
+
+$number = 7;
+echo ++$number; // $number=8
+echo "<br>";
+
+$number = 7;
+echo $number--; // $number=7
+echo "<br>";
+
+$number = 7;
+echo --$number; // $number=6
+
+?>
