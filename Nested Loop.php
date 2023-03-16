294a14b8c4f8bca5fef8c9f016bad3d66ab0be45
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-16 18:27:04 +0600

    Create Nested Loop.php

---
+++ Nested Loop.php
@@ -0,0 +1,14 @@
+<?php
+
+for ($outerCounter=1; $outerCounter <= 10; $outerCounter++) {
+	echo "List: $outerCounter<br>";
+
+	for ($interCounter=0; $interCounter <=15; $interCounter++) { 
+		echo "$interCounter ";
+	}
+	
+	echo "<br>";
+}
+
+?>
+
