621330d101b982f7dfe339d6a74061b0c8eb4b6d
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-16 19:36:47 +0600

    Create function.php

---
+++ function.php
@@ -0,0 +1,19 @@
+<?php
+
+ myName();
+ echo "<br>";
+ myAge();
+ echo "<br>";
+ myGender();
+
+ function myName(){
+ 	echo "Md. Rakibul Islam";
+ }
+ function myAge(){
+ 	echo "23";
+ }
+ function myGender(){
+ 	echo "Male";
+ }
+ 
+?>
