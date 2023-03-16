ee9c54d0f01c7f9913bfbe3734a406d9e96fa044
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-16 19:22:55 +0600

    Create isset.php

---
+++ isset.php
@@ -0,0 +1,14 @@
+<?php
+
+ $first_number = 30;
+ $second_number = 20;
+ $value = null;
+
+ if (isset($value)) {
+ 	$result = first_number + value;
+ }else{
+ 	$result = 5;
+ }
+ echo "$result";
+?>
+
