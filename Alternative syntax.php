494af4bedce18d3c3ccdafca994829baff4732c2
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-16 18:50:36 +0600

    Create Alternative syntax.php

---
+++ Alternative syntax.php
@@ -0,0 +1,15 @@
+<?php
+
+ $first_number = 30;
+ $second_number = 20;
+
+ if ($first_number==10):
+ 	echo "True";
+ elseif ($second_number==10):
+ 	echo "True";
+ else:
+ 	echo "False";
+ endif;
+
+?>
+
