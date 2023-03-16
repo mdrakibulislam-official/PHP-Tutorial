dd1260b72dcc40249afa87ed3a488f9e3a899759
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-16 14:51:08 +0600

    Create if_else.php

---
+++ if_else.php
@@ -0,0 +1,12 @@
+<?php
+
+$first_number = 10;
+$second_number = 20;
+
+if ($first_number > $second_number) {
+	echo "The first number is greater than the second number.";
+}else{
+	echo "The second number is greater than the first number.";
+}
+
+?>
