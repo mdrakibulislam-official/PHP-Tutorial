69c52f7430ed19e63e88dc9969028fb469d08bf7
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-16 14:45:49 +0600

    Create Comparison-operator.php

---
+++ Comparison-operator.php
@@ -0,0 +1,42 @@
+<?php
+
+$first_number = 10;
+$second_number = 20;
+
+$result = $first_number < $second_number;
+var_dump($result);
+echo "<br>";
+
+$result = $first_number > $second_number;
+var_dump($result);
+echo "<br>";
+
+$result = $first_number <= $second_number;
+var_dump($result);
+echo "<br>";
+
+$result = $first_number >= $second_number;
+var_dump($result);
+echo "<br>";
+
+$result = $first_number <> $second_number;
+var_dump($result);
+echo "<br>";
+
+$result = $first_number == $second_number;
+var_dump($result);
+echo "<br>";
+
+$result = $first_number === $second_number;
+var_dump($result);
+echo "<br>";
+
+$result = $first_number != $second_number;
+var_dump($result);
+echo "<br>";
+
+$result = $first_number <=> $second_number;
+var_dump($result);
+echo "<br>";
+
+?>
