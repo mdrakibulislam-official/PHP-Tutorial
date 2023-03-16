64fbf048c4324d8a077330bd4463f538b164e32f
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-16 18:07:33 +0600

    Create User list using for loop.php

---
+++ User list using for loop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User List</title>
+</head>
+<body>
+	<ul>
+		<?php
+		for ($i=1; $i <=20; $i++) { 
+			echo "<li>User: $i</li>";
+		}
+		?>
+
+	</ul>
+
+</body>
+</html>
