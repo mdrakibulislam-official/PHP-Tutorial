96d5cd7f9316aabbd410e6f61fd950db164de89d
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-16 18:45:09 +0600

    Create sprintf.php

---
+++ sprintf.php
@@ -0,0 +1,10 @@
+<?php
+
+ $first_number = 10;
+ $second_number = 20;
+
+ $new_string = sprintf("The first number = %s<br>The second number = %s",$first_number, $second_number);
+
+ echo $new_string;
+
+?>
