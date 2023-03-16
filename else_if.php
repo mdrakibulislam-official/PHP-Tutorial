ef88829f39a969d3d4ff7b665e557d0f13400994
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-03-16 14:55:57 +0600

    Create else_if.php

---
+++ else_if.php
@@ -0,0 +1,21 @@
+<?php
+
+$marks = 69;
+
+if ($marks <33) {
+	echo "The grade is = F.";
+}else if ($marks <40) {
+	echo "The grade is = D.";
+}else if ($marks <50) {
+	echo "The grade is = C.";
+}else if ($marks <60) {
+	echo "The grade is = B.";
+}else if ($marks <70) {
+	echo "The grade is = A-.";
+}else if ($marks <80) {
+	echo "The grade is = A.";
+}else{
+	echo "The grade is = A+.";
+}
+
+?>
